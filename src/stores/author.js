91f28325803ebeb2996e01f4c28d06086ac22cc1
Derrick Strong <derrickmstrong@gmail.com>
2024-03-23 13:23:46 -0500

    Refactor author and post stores

--- src/stores/author.js
+++ src/stores/author.js
@@ -2,17 +2,17 @@ import { defineStore } from 'pinia';
 import { usePostStore } from './post';
 
 export const useAuthorStore = defineStore({
-  id: 'author',
-  state: () => ({
-    authors: [],
-  }),
-  getters: {
-    getPostAuthor: (state) => state.authors.find((author) => author.id === usePostStore().post.userId),
-  },
-  actions: {
-    async fetchAuthors() {
-      this.authors = await fetch('https://jsonplaceholder.typicode.com/users')
-      .then((res) => res.json())
+    id: 'author',
+    state: () => ({
+        authors: [],
+    }),
+    getters: {
+        getPostAuthor: (state) => state.authors.find((author) => author.id === usePostStore().post.userId),
+    },
+    actions: {
+        async fetchAuthors() {
+        this.authors = await fetch('https://jsonplaceholder.typicode.com/users')
+        .then((res) => res.json())
+        }
     }
-  }
 })
--- src/stores/post.js
+++ src/stores/post.js
@@ -1,41 +1,41 @@
 import { defineStore } from 'pinia'
 
 export const usePostStore = defineStore({
-  id: 'post',
-  state: () => ({
-    posts: [],
-    post: null,
-    loading: false,
-    error: null,
-  }),
-  getters: {
-    getPostsPerAuthor: (state) => { 
-      return (authorId) => state.posts.filter((post) => post.userId === authorId )
-     }
-  },
-  actions: {
-    async fetchPosts() {
-      this.posts = [];
-      this.loading = true;
-      try {
-        this.posts = await fetch(`https://jsonplaceholder.typicode.com/posts/`).then((res) => res.json() );
-      } catch (err) {
-        this.error = err;
-      } finally {
-        this.loading = false;
+    id: 'post',
+    state: () => ({
+      posts: [],
+      post: null,
+      loading: false,
+      error: null,
+    }),
+    getters: {
+      getPostsPerAuthor: (state) => { 
+        return (authorId) => state.posts.filter((post) => post.userId === authorId )
       }
     },
-    async fetchPost(id) {
-      this.post = null;
-      this.loading = true;
-      try {
-        this.post = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`).then((res) => res.json() );
-      } catch (err) {
-        this.error = err;
-      } finally {
-        this.loading = false;
+    actions: {
+      async fetchPosts() {
+        this.posts = [];
+        this.loading = true;
+        try {
+          this.posts = await fetch(`https://jsonplaceholder.typicode.com/posts/`).then((res) => res.json() );
+        } catch (err) {
+          this.error = err;
+        } finally {
+          this.loading = false;
+        }
+      },
+      async fetchPost(id) {
+        this.post = null;
+        this.loading = true;
+        try {
+          this.post = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`).then((res) => res.json() );
+        } catch (err) {
+          this.error = err;
+        } finally {
+          this.loading = false;
+        }
       }
     }
-  }
 })
 
